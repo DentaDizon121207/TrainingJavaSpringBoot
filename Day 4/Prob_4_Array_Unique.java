625ea3f534367cd0cc4b9c904da24ebcb38d0e54
Denny Muharom Ariawan <dennymuharomariawan@gmail.com>
2022-07-20 09:03:26 +0700

    Tugas CLI day 2

--- Day 4/Prob_4_Array_Unique.java
+++ Day 4/Prob_4_Array_Unique.java
@@ -4,16 +4,16 @@ public class Prob_4_Array_Unique
 {    
     public static void main(String[] args)
     {
-        ArrayList<Integer> arr1 = new ArrayList<>(Arrays.asList(1,2,3,4));
-        ArrayList<Integer> arr2 = new ArrayList<>(Arrays.asList(1,3,5,10,16));
+        ArrayList<Integer> ListArray1 = new ArrayList<>(Arrays.asList(1,2,3,4));
+        ArrayList<Integer> ListArray2 = new ArrayList<>(Arrays.asList(1,3,5,10,16));
         ArrayList<Integer> result = new ArrayList<Integer>();
 
-        System.out.println("Input : " + arr1 + " dan " + arr2);
+        System.out.println("Input : " + ListArray1 + " dan " + ListArray2);
  
-        for (int i = 0; i <= arr1.size() - 1; i++)
+        for (int i = 0; i <= ListArray1.size() - 1; i++)
         {
-            if(!arr2.contains(arr1.get(i)))
-                result.add(arr1.get(i));
+            if(!ListArray2.contains(ListArray1.get(i)))
+                result.add(ListArray1.get(i));
         }
 
         System.out.println("Output : " + result);
