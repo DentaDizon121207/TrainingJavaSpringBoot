9ab69ce9ed6803de11c61659f21af81da9fa354b
Denny Muharom Ariawan <dennymuharomariawan@gmail.com>
2022-08-03 20:41:49 +0700

    Tugas Day 15

---
+++ Day 15/Tugas15Denny/Tugas15Denny/src/test/java/com/Day15Denny/Tugas15Denny/Tugas15DennyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Day15Denny.Tugas15Denny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tugas15DennyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
